0fe261d88170650274e5762ed2a61c7f2a163e29
Liubomyr86 <demianchukliubomyr@gmail.com>
2021-03-11 00:06:26 +0100

    feat: implement function toReadable that converts given number, to readable string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function toReadable (number) {
-  
+    const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let string = number.toString();
+    let word;
+    let words = [];
+
+    if (parseInt(string) === 0) return 'zero';
+
+    if(string) {
+        let integer = string.split('').reverse().map(parseFloat);
+
+        if (integer[1] === 1) integer[0] += 10;
+
+        if (word = units[integer[0]]) words.push(word);
+
+        if (word = tens[integer[1]]) words.push(word);
+
+        if (word = units[integer[2]]) words.push(word + ' hundred');
+    }
+
+    return words.reverse().join(' ');
 }
